004d95de536008c465664f38fd002808e56bb757
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 17:01:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,7 +2,7 @@ magic_exit_number = 0
 count = 10 
 while count > magic_exit_number do
   break if count == magic_exit_number
-  puts "#{count}"
+  puts count
   count -= 1
 end
 puts "Happy New Year!"
