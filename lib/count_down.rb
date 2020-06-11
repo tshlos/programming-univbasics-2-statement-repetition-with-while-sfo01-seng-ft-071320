36d785b60beba18f8c775c64f8bc0837167ae361
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 17:00:32 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
-# Write your code here
+magic_exit_number = 0
+count = 10 
+while count > magic_exit_number do
+  break if count == magic_exit_number
+  puts "#{count}"
+  count -= 1
+end
+puts "Happy New Year!"
